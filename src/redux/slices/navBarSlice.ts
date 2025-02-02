822f37f483c416a207355d20e63c05c9e7393c0c
Darién Muiño Martín <37383949+dmuino1986@users.noreply.github.com>
2025-02-02 15:19:45 -0500

    working on redux

---
+++ src/redux/slices/navBarSlice.ts
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface NavbarState {
+  isVisible: boolean;
+}
+
+const initialState: NavbarState = {
+  isVisible: false,
+};
+
+const navbarSlice = createSlice({
+  name: "navbar",
+  initialState,
+  reducers: {
+    showNavbar: (state) => {
+      state.isVisible = true;
+    },
+    hideNavbar: (state) => {
+      state.isVisible = false;
+    },
+  },
+});
+
+export const { showNavbar, hideNavbar } = navbarSlice.actions;
+export default navbarSlice.reducer;
